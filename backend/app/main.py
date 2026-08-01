0df4d0cf639ad34c0bc86a76ac8453abf8209bed
krrish2803 <kyaduka2803@gmail.com>
2026-08-01 16:03:57 +0530

    fix: change CORS setup to use wildcard allow_origins for bulletproof domain support

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,14 +40,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "http://localhost:8002",
-        "http://127.0.0.1:8002",
-        "https://forge-room.netlify.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
